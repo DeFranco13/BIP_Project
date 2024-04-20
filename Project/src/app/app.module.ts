45da8913324aa58232413f71425339b60347f094
Jordi <jordifransen@hotmail.com>
2024-04-20 18:58:12 +0200

    Start dev phase

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { InputComponent } from './input/input.component';
 import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     InputComponent,
     ResultComponent
   ],
