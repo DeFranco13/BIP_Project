1e72e7863199be20aee5b1f21b052e3d88f93d33
Jordi <jordifransen@hotmail.com>
2024-05-14 15:34:56 +0200

    Backend Update

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { JsonService } from '../services/serverCall';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent {
-  testArray: any; 
+  boolCalculator = false;
+  boolResults = false;
+  JsonArray: any;
+  testArrayServer: any; 
 
   constructor(private jsonService: JsonService, private http: HttpClient) {}
   
@@ -16,12 +20,30 @@ export class CalculatorComponent {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
   SubmitResults(){
-    
+
   }
-  StartC() {
+  startResults(){
+    this.boolResults = true
+  }
+
+  StartCalculator(){
+    this.boolCalculator = true
+    this.LoadJson()
+  }
+
+  LoadJson(){
+    this.JsonArray = this.jsonService.getJsonLocal()
+    console.log(this.jsonService)
+  }
+  
+  getObjectKeys(obj: any): string[] {
+    return Object.keys(obj);
+  }
+  LoadJsonServer() {
+    // Ignore this, its for testing the backend
     this.jsonService.getJsonData().subscribe(data => {
-      this.testArray = data;
-      console.log(this.testArray);
+      this.testArrayServer = data;
+      console.log(this.testArrayServer[0]);
     });
   }
   
--- src/app/services/serverCall.ts
+++ src/app/services/serverCall.ts
@@ -1,13 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as data from '../../assets/questions.json'
 
 @Injectable({
   providedIn: 'root'
 })
 export class JsonService {
   constructor(private http: HttpClient) { }
+  
 
   getJsonData() {
     return this.http.get('http://49.13.215.13:8080/file.json');
+  }
+  getJsonLocal(){
+    return data
   }
 }
