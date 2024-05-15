26650afe12e849840fc8e3ee652366fd2ed5c665
Jordi <jordifransen@hotmail.com>
2024-05-15 09:53:11 +0200

    Backend update

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -12,7 +12,8 @@ export class CalculatorComponent {
   boolCalculator = false;
   boolResults = false;
   JsonArray: any;
-  testArrayServer: any; 
+  keyArray = ["Health", "Value", "Society & Culture", "Biodiversity", "Water", "Energy", "Materials"]
+  currentKey: number = 0
 
   constructor(private jsonService: JsonService, private http: HttpClient) {}
   
@@ -30,22 +31,24 @@ export class CalculatorComponent {
     this.boolCalculator = true
     this.LoadJson()
   }
-
+  getCurrentPillar(){
+    return this.keyArray[this.currentKey]
+  }
+  addCurrentKey(){
+    this.currentKey += 1
+  }
+  getCurrentKey(){
+    return this.currentKey
+  }
   LoadJson(){
     this.JsonArray = this.jsonService.getJsonLocal()
     console.log(this.jsonService)
   }
-  
+
   getObjectKeys(obj: any): string[] {
     return Object.keys(obj);
   }
-  LoadJsonServer() {
-    // Ignore this, its for testing the backend
-    this.jsonService.getJsonData().subscribe(data => {
-      this.testArrayServer = data;
-      console.log(this.testArrayServer[0]);
-    });
-  }
+  
   
 }
 
