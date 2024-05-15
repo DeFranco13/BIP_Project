29044b753ba895c6942060cfeff83edc353c69dd
MaksymTaran25 <masimo2801@gmail.com>
2024-05-16 01:50:23 +0300

    added guide page, added some styles

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -12,7 +12,7 @@ export class CalculatorComponent {
   boolCalculator = false;
   boolResults = false;
   JsonArray: any;
-  keyArray = ["Health", "Value", "Society & Culture", "Biodiversity", "Water", "Energy", "Materials"]
+  keyArray = ["Company", "Materials", "Energy", "Water", "Biodiversity", "Society & Culture", "Health", "Value"]
   currentKey: number = 0
 
   constructor(private jsonService: JsonService, private http: HttpClient) {}
@@ -30,6 +30,9 @@ export class CalculatorComponent {
   StartCalculator(){
     this.boolCalculator = true
     this.LoadJson()
+    
+    let button = <HTMLButtonElement> document.getElementById('button1');
+    button.remove()
   }
   getCurrentPillar(){
     return this.keyArray[this.currentKey]
